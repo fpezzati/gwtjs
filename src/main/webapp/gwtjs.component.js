dbc2fc3077b216530adc9fde10f15f2624f46c5d
Francesco Pezzati <PEFR@maior.local>
2020-03-05 18:13:04 +0100

    Not working.

---
+++ src/main/webapp/gwtjs.component.js
@@ -0,0 +1,13 @@
+window.customElements.define('effort-component', class EffortComponent extends HTMLElement {
+	constructor() {
+		super();
+		this._shadowRoot = this.attachShadow({ 'mode': 'open' });
+	}
+
+	connectedCallback() {
+    	console.log('connected!');
+		var effortTable = edu.pezzati.gwtjs.client.provided.EffortTable();
+		this._shadowRoot.appendChild(effortTable);
+    });
+  }
+});
